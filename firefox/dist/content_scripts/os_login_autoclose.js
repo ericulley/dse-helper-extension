3d6188162c7fe03a44d3ef5c07577c972bee0785
Eric Culley <eric.culley@auth0.com>
2023-08-11 10:30:07 -0600

    added comment

--- firefox/dist/content_scripts/os_login_autoclose.js
+++ firefox/dist/content_scripts/os_login_autoclose.js
@@ -1,6 +1,11 @@
 "use strict";
 const path = window.location.pathname;
 const query = window.location.search;
+/*
+*  Current Firefox versions seem to block this content
+*  script, possibly due to the Content-Security-Policy
+*  on the https://*.pop.prod.a0core.net domains.
+*/
 console.log("Autoclose content script start");
 if (path.length <= 1 && query.length <= 1) {
     browser.runtime.sendMessage({ path: '/api/os-login-autoclose' }).then((res) => {
